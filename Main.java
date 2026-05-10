3db01be23812b07c7e12a47da589360c94ee5c12
Mohit Kumar <mohitkr12singh@gmail.com>
2026-05-10 22:29:59 +0530

    Added Student class and ArrayList feature

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+
 class Main {
+
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Student Management System");
+        // Creating ArrayList
+        ArrayList<Student> students = new ArrayList<>();
 
+        // Creating Student Objects
+        Student s1 = new Student(101, "Mohit", 85.5);
+        Student s2 = new Student(102, "Rahul", 90.0);
+
+        // Adding students to ArrayList
+        students.add(s1);
+        students.add(s2);
+
+        // Displaying all students
+        for (Student student : students) {
+            student.displayStudent();
+        }
     }
 }
